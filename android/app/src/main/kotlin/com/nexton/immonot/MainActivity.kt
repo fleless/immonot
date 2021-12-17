2f0b7028305ea1c90e24caf0357397d6620e4f7f
Amanallah Trifi <t.amanallah1992@gmail.com>
2021-12-17 15:14:20 +0100

    "push notifs android implemented"

--- android/app/src/main/kotlin/com/nexton/immonot/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.nexton.immonot
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/modules/app/app_widget.dart
+++ lib/modules/app/app_widget.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:io';
 
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -29,6 +31,36 @@ class _AppWidgetState extends State<AppWidget> {
               statusBarBrightness: Brightness.light,
               statusBarIconBrightness: Brightness.dark,
             )));
+
+    /// handling push notifications functions
+    _initFlutterDownloaderPackage();
+    initialize(context);
+  }
+
+  _initFlutterDownloaderPackage() async {
+    ///when app is terminated or closed gives you the message on which user taps
+    FirebaseMessaging.instance.getInitialMessage().then((message) {});
+
+    ///Only work on Foreground listener
+    FirebaseMessaging.onMessage.listen((event) {});
+
+    ///when app is in background but opened and user taps on the notification
+    FirebaseMessaging.onMessageOpenedApp.listen((event) {});
+  }
+
+  Future initialize(context) async {
+    final FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+    if (Platform.isIOS) {
+      firebaseMessaging.requestPermission(
+        alert: true,
+        announcement: false,
+        badge: true,
+        carPlay: false,
+        criticalAlert: false,
+        provisional: false,
+        sound: true,
+      );
+    }
   }
 
   @override
